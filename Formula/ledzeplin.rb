8171f4d1eff044d3206c3347e3fa03e9b643e739
azhsetiawan <azhsetiawan@users.noreply.github.com>
2020-10-02 13:22:40 +0700

    update formula ledzeplin

--- Formula/ledzeplin.rb
+++ Formula/ledzeplin.rb
@@ -1,8 +1,8 @@
 class Ledzeplin < Formula
   desc "Zeplin multi account switcher"
   homepage "https://github.com/azhsetiawan/ledzeplin"
-  url "https://github.com/azhsetiawan/ledzeplin/releases/download/v0.1.0/ledzeplin.tar.gz"
-  sha256 "d1940818b8b708159108ae6d7f9f0e5d59bd65abebebfc0b6297dedc4922f5b5"
+  url "https://github.com/azhsetiawan/ledzeplin/releases/download/v0.1.1/ledzeplin.tar.gz"
+  sha256 "b62854b60380a777e86c1b29a5f02009cb9fb1c0c054a851b3215aa094dfb8b9"
   license "MIT"
 
   bottle :unneeded
